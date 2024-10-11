0ad3f1aac30b21cbf888edcd0f55a7cfdd870196
M-A-Noman <mdabunomanma@gmail.com>
2024-10-11 12:13:46 +0600

    primary setup for library with story book

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,6 @@
+import { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">;
+export const Button = ({ ...props }: ButtonProps) => {
+  return <button className="text-blue-500" {...props} />;
+};
